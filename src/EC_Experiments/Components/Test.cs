743393864c3ac4273f1833208721433da2780b79
TacoConKvass <e_frun@o2.pl>
2025-06-03 18:05:39 +0200

    [Add] On hit player functionality for Test component

--- src/EC_Experiments/Components/Test.cs
+++ src/EC_Experiments/Components/Test.cs
@@ -1,18 +1,33 @@
 using EC.Core;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-using ComponentData = (int DustType, int Amount);
+using ComponentData = (int DustType, int Amount, Microsoft.Xna.Framework.Vector2 Range);
 
 namespace EC_Experiments.Components;
 
 public class Test {
+	public static readonly ComponentData Invalid = new();
+
 	[ReinitializeDuringResizeArrays]
 	public static class Values {
-		public static ComponentData[] Items = ItemID.Sets.Factory.CreateNamedSet("EC/Test").RegisterCustomSet(new ComponentData(), ItemID.CopperAxe, new ComponentData(DustID.Water, 20));
-		public static ComponentData[] Projectiles = ProjectileID.Sets.Factory.CreateNamedSet("EC/Test").RegisterCustomSet(new ComponentData(), ProjectileID.Bullet, new ComponentData(DustID.Ice, 20));
-		public static ComponentData[] NPCs = NPCID.Sets.Factory.CreateNamedSet("EC/Test").RegisterCustomSet<ComponentData>(new());
+		public static ComponentData[] Items = ItemID.Sets.Factory.CreateNamedSet("EC/Test")
+			.Description("Test component - spawns Dust on target hit")
+			.RegisterCustomSet(new ComponentData(), 
+				ItemID.CopperAxe, new ComponentData(DustID.Water, 20, new Vector2(20))
+			);
+
+		public static ComponentData[] Projectiles = ProjectileID.Sets.Factory.CreateNamedSet("EC/Test")
+			.Description("Test component - spawns Dust on target hit")
+			.RegisterCustomSet(new ComponentData(), 
+				ProjectileID.Bullet, new ComponentData(DustID.Ice, 20, new Vector2(20))
+			);
+
+		public static ComponentData[] NPCs = NPCID.Sets.Factory.CreateNamedSet("EC/Test")
+			.Description("Test component - spawns Dust on target hit")
+			.RegisterCustomSet<ComponentData>(new());
 	}
 
 	public class Items : GlobalItem, IComponent<ComponentData> {
@@ -26,7 +41,11 @@ public class Test {
 		}
 
 		public override void OnHitNPC(Item item, Player player, NPC target, NPC.HitInfo hit, int damageDone) {
-			if (Enabled) Execute_NPC(Data, target);
+			if (Enabled) Execute(Data, target.Center);
+		}
+
+		public override void OnHitPvp(Item item, Player player, Player target, Player.HurtInfo hurtInfo) {
+			if (Enabled) Execute(Data, target.Center);
 		}
 	}
 
@@ -41,7 +60,11 @@ public class Test {
 		}
 
 		public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone) {
-			if (Enabled) Execute_NPC(Data, target);
+			if (Enabled) Execute(Data, target.Center);
+		}
+
+		public override void OnHitPlayer(Projectile projectile, Player target, Player.HurtInfo info) {
+			if (Enabled) Execute(Data, target.Center);
 		}
 	}
 	public class NPCs : GlobalNPC, IComponent<ComponentData> {
@@ -51,17 +74,21 @@ public class Test {
 
 		public override void SetDefaults(NPC entity) {
 			Data = Values.NPCs[entity.type];
-			Enabled = (Data == new ComponentData());
+			Enabled = (Data != new ComponentData());
 		}
 
 		public override void OnHitNPC(NPC npc, NPC target, NPC.HitInfo hit) {
-			if (Enabled) Execute_NPC(Data, target);
+			if (Enabled) Execute(Data, target.Center);
+		}
+
+		public override void OnHitPlayer(NPC npc, Player target, Player.HurtInfo hurtInfo) {
+			if (Enabled) Execute(Data, target.Center);
 		}
 	}
 
-	public static void Execute_NPC(ComponentData data, NPC target) {
+	public static void Execute(ComponentData data, Vector2 position) {
 		for (int i = 0; i < data.Amount; i++) {
-			Dust.NewDust(target.Center, target.width, target.height, data.DustType);
+			Dust.NewDust(position, (int)data.Range.X, (int)data.Range.Y, data.DustType);
 		}
 	}
 }

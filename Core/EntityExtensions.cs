21d3f87a4a762f930f8f5202e573c6c3e0a770b5
TacoConKvass <e_frun@o2.pl>
2025-06-14 08:38:24 +0200

    [Add] `Disable<T>` for all entities

--- Core/EntityExtensions.cs
+++ Core/EntityExtensions.cs
@@ -6,24 +6,49 @@ public static class EntityExtensions
 {
 	public static void Enable<T>(this NPC entity) where T : NPCComponent {
 		NPCComponent component = entity.GetGlobalNPC<T>();
+		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
-		component.OnEnabled(entity);
 	}
+
 	public static void Enable<T>(this Projectile entity) where T : ProjectileComponent {
 		ProjectileComponent component = entity.GetGlobalProjectile<T>();
+		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
-		component.OnEnabled(entity);
 	}
 
 	public static void Enable<T>(this Item entity) where T : ItemComponent {
-		NPCComponent component = entity.GetGlobalItem<T>();
+		ItemComponent component = entity.GetGlobalItem<T>();
+		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
-		component.OnEnabled(entity);
 	}
 
 	public static void Enable<T>(this Player entity) where T : PlayerComponent {
-		NPCComponent component = entity.GetModPlayer<T>();
+		PlayerComponent component = entity.GetModPlayer<T>();
+		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
-		component.OnEnabled(entity);
+	}
+
+	public static void Disable<T>(this NPC entity) where T : NPCComponent {
+		NPCComponent component = entity.GetGlobalNPC<T>();
+		if (component.Enabled) component.OnDisabled(entity);
+		component.Enabled = false;
+	}
+
+	public static void Disable<T>(this Projectile entity) where T : ProjectileComponent {
+		ProjectileComponent component = entity.GetGlobalProjectile<T>();
+		if (component.Enabled) component.OnDisabled(entity);
+		component.Enabled = false;
+	}
+
+	public static void Disable<T>(this Item entity) where T : ItemComponent {
+		ItemComponent component = entity.GetGlobalItem<T>();
+		if (component.Enabled) component.OnDisabled(entity);
+		component.Enabled = false;
+	}
+
+	public static void Disable<T>(this Player entity) where T : PlayerComponent {
+		PlayerComponent component = entity.GetModPlayer<T>();
+		if (component.Enabled) component.OnDisabled(entity);
+		component.Enabled = false;
 	}
 }

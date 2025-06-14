05ff91fa8459b268ff3dc1eca3bbdbc00af62abc
TacoConKvass <e_frun@o2.pl>
2025-06-14 11:29:54 +0200

    [Change] Enable<T> now returns T

--- Core/EntityExtensions.cs
+++ Core/EntityExtensions.cs
@@ -4,28 +4,32 @@ namespace EC.Core;
 
 public static class EntityExtensions
 {
-	public static void Enable<T>(this NPC entity) where T : NPCComponent {
+	public static T Enable<T>(this NPC entity) where T : NPCComponent {
 		NPCComponent component = entity.GetGlobalNPC<T>();
 		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
+		return (T)component;
 	}
 
-	public static void Enable<T>(this Projectile entity) where T : ProjectileComponent {
+	public static T Enable<T>(this Projectile entity) where T : ProjectileComponent {
 		ProjectileComponent component = entity.GetGlobalProjectile<T>();
 		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
+		return (T)component;
 	}
 
-	public static void Enable<T>(this Item entity) where T : ItemComponent {
+	public static T Enable<T>(this Item entity) where T : ItemComponent {
 		ItemComponent component = entity.GetGlobalItem<T>();
 		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
+		return (T)component;
 	}
 
-	public static void Enable<T>(this Player entity) where T : PlayerComponent {
+	public static T Enable<T>(this Player entity) where T : PlayerComponent {
 		PlayerComponent component = entity.GetModPlayer<T>();
 		if (!component.Enabled) component.OnEnabled(entity);
 		component.Enabled = true;
+		return (T)component;
 	}
 
 	public static void Disable<T>(this NPC entity) where T : NPCComponent {
